8ede06204701746201685e79c2c13cac0fa135db
alphalemon <g.diblas@email.it>
2012-06-17 21:55:21 +0200

    renamed ajax_error to ajax_dialog [ci skip]

--- Core/Tools/AlToolkit.php
+++ Core/Tools/AlToolkit.php
@@ -10,9 +10,9 @@
  * file that was distributed with this source code.
  *
  * For extra documentation and help please visit http://alphalemon.com
- * 
+ *
  * @license    MIT License
- * 
+ *
  */
 
 namespace AlphaLemon\PageTreeBundle\Core\Tools;
@@ -23,6 +23,8 @@ use Symfony\Component\Console\Input\StringInput;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
 
 /**
  * A set of useful methods
@@ -31,51 +33,31 @@ class AlToolkit
 {
     /**
      * Executes a command
-     * 
+     *
      * @param KernelInterface $kernel
      * @param string/array $commands
      * @return boolean
      */
-    public static function executeCommand(KernelInterface $kernel = null, $commands = array())
+    public static function executeCommand($appDir, $cmd)
     {
-        if(null !== $kernel && !empty($commands))
-        {
-            if(!is_array($commands)) $commands = array($commands);
-        
-            $application = new Application($kernel);
-            $application->setCatchExceptions(false);
-            $application->setAutoExit(false);
-            
-            foreach($commands as $command)
-            {
-                if(preg_match('/^cache:clear/', $command)) {
-                    $command .= ' --no-warmup';
-                }
+        $phpFinder = new PhpExecutableFinder();
+        $php = escapeshellarg($phpFinder->find());
+        $console = escapeshellarg($appDir . '/console');
 
-                $input = new StringInput($command);
-                $input->setInteractive(false);
-                $res = $application->run($input, null);
-                if($res != 0)
-                {
-                    return $res;
-                }
-            }
-            unset($application);
-        }
-        
-        return false;
+        $process = new Process($php.' '.$console.' '.$cmd);
+        $process->run();
     }
-    
+
     /**
      * Translates a message from the container
-     * 
+     *
      * @deprecated
-     * 
+     *
      * @param ContainerInterface $container
      * @param string $message
      * @param array $tokens
      * @param string $dictionary
-     * @return string 
+     * @return string
      */
     public static function translateMessage(ContainerInterface $container, $message, array $tokens = array(), $dictionary = '')
     {
@@ -87,17 +69,17 @@ class AlToolkit
                 return $translator->trans($message, $tokens, $dictionary);
             }
         }
-        
+
         return $message;
     }
-    
+
     /**
      * Retrieves the web folder from the given bundle
-     * 
+     *
      * @param ContainerInterface $container
      * @param string $bundleName
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public static function retrieveBundleWebFolder(KernelInterface $kernel, $bundleName)
     {
@@ -117,22 +99,22 @@ class AlToolkit
 
     /**
      * Locates a resource and returns its absolute path
-     * 
+     *
      * @param ContainerInterface    $container
      * @param string                $path
-     * 
-     * @return string  , $forceRelativePath = false $forceRelativePath && 
+     *
+     * @return string  , $forceRelativePath = false $forceRelativePath &&
      */
     public static function locateResource(KernelInterface $kernel, $path)
     {
         if(\substr($path, 0, 1) != '@') $path = '@' . $path;
-        
+
         $fullPath = "";
         if('@' === \substr($path, 0, 1))
         {
             try
             {
-                $fullPath = $kernel->locateResource($path); 
+                $fullPath = $kernel->locateResource($path);
             }
             catch(\InvalidArgumentException $e)
             {
@@ -143,21 +125,21 @@ class AlToolkit
         {
             $fullPath = $path;
         }
-        
+
         return $fullPath;
     }
-    
+
     /**
      * Normalize a path to a unix path
-     * 
+     *
      * @param   string      $path
-     * @return  string 
+     * @return  string
      */
-    public static function normalizePath($path) 
+    public static function normalizePath($path)
     {
         return preg_replace('/\\\/', '/', $path);
     }
-    
+
     /**
      * Set a string which has a leng of a number of characters predefined. If string is lengther
      * than the width we need, it will be reduced to max width and three point will be added.
@@ -172,44 +154,44 @@ class AlToolkit
     }
 
     /**
-     * Extracts a zip file to a given path 
+     * Extracts a zip file to a given path
      *
      * @param      str The zip file
      * @param      str The destination path
-     * 
-     * @return     int 
-     */ 
+     *
+     * @return     int
+     */
     public static function extractZipFile($zipFile, $destinationPath)
-    {   
+    {
         $zipLibrary = __DIR__ . "/../../vendors/Zip/zipfile.php";
         if(@require_once($zipLibrary))
         {
             $zip = new \ZipArchive();
             if ($zip->open($zipFile) === true)
-            { 
+            {
                 $result = $zip->extractTo($destinationPath);
                 $zip->close();
 
-                return $result; 
-            } 
+                return $result;
+            }
             else
             {
                 return 0;
             }
         }
-        else 
+        else
         {
             throw new \Exception(sprintf("The zip library cannot be loaded because %s is not a valid path", $zipLibrary));
         }
     }
-    
+
     /**
      * Slugifies a path
-     * 
-     * Based on http://php.vrana.cz/vytvoreni-pratelskeho-url.php 
-     * 
+     *
+     * Based on http://php.vrana.cz/vytvoreni-pratelskeho-url.php
+     *
      * @param type $text
-     * @return type 
+     * @return type
      */
     static public function slugify($text)
     {
@@ -235,16 +217,16 @@ class AlToolkit
 
         return $text;
     }
- 
+
     /**
      * Retrieves the mime type for the given resource
-     * 
+     *
      * @param   string $filename
-     * @return string 
+     * @return string
      */
     public static function mimeContentType($filename)
     {
-        $result = new \finfo();        
+        $result = new \finfo();
         return $result->file($filename, FILEINFO_MIME_TYPE);
     }
 }
